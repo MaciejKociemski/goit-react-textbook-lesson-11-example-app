ff4cf040f02b33763628f54b92d039ff6f18b895
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-06-27 12:01:06 +0200

    feat: bug fix and correct code from lesson
    its working

---
+++ src/components/TaskForm/TaskForm.jsx
@@ -0,0 +1,25 @@
+import css from './TaskForm.module.css';
+
+import { useDispatch } from 'react-redux';
+import { addTask } from 'redux/tasksSlice';
+export const TaskForm = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    dispatch(addTask(form.elements.text.value));
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input
+        className={css.field}
+        type="text"
+        name="text"
+        placeholder="Enter task text..."
+      />
+      <button type="submit">Add task</button>
+    </form>
+  );
+};
